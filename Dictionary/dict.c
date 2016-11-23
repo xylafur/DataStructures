0bdee7fc5efcab839d29e7e22335b13e1e7bae7b
xylafur <jkesleyrichardson@gmail.com>
2016-11-23 17:34:54 -0600

    added assign function for the dict

--- Dictionary/dict.c
+++ Dictionary/dict.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DICTSIZE 10
 
@@ -32,16 +33,53 @@ int hash(char *inputS){
     sum += (*inputS - '0');
     inputS++;
   }
-  printf("The sum value is: %d\n", sum);
   return (sum % DICTSIZE);
 }
 
+//function to assign a value to our dict array
+//  returns 1 if the element already existed, 0
+//  if the element had to be added
+int assign(dict* pD, int index, char *inKey, int inVal){
+  //if the dict has not been assigned the key will have
+  //  a value == to NULL
+  if(pD[index].key == NULL){
+    pD[index].key = inKey;
+    pD[index].value = inVal;
+    return 0;
+  }
+  //if the specific index has already been assigned a
+  //  key and value then we must make sure that it is the
+  //  final node at that specific index
+
+  dict *temp = &pD[index];    //temp to go through linked list
+  while(temp->pNext != NULL){
+
+    //this means the key already exists
+    if(strcmp(temp->key, inKey) == 1){
+      temp->value = inVal;
+      return 1;
+    }
+    temp = temp->pNext;
+  }
+  temp->pNext = malloc(sizeof(dict));
+  temp = temp->pNext;
+
+  temp->value = inVal;
+  temp->key = inKey;
+
+  return 1;
+}
+
+
 int main(){
 
-  dict *D = malloc( sizeof(dict) * DICTSIZE);
-  int i = hash("Kesley");
+  dict *D = calloc(DICTSIZE, sizeof(dict));
 
-  printf("%d", i);
+  assign(D, hash("Kesley"), "Kesley", 7);
+
+  printf("%s", D->key);
+  printf("%d", D->key==NULL);
+
 
 
   return 0;
