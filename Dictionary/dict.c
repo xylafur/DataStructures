0a75802ee4049c2ea5648b36e4a91b2d8f1551b5
xylafur <jkesleyrichardson@gmail.com>
2016-11-23 19:13:38 -0600

    added contains and value functions

--- Dictionary/dict.c
+++ Dictionary/dict.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DICTSIZE 10
+//if we go the rout of % we should use a prime # for size
+#define DICTSIZE 7
 
 /**************************************************
 **********************HEADER***********************
@@ -15,6 +16,10 @@
 * will simply sum up the key (which can only be   *
 * a string in this case) and the preform modlo    *
 * on the result with the DICTSIZE macro.          *
+*                                                 *
+* If you want to actually use this dictinary be   *
+* aware that if you search for a key that is not  *
+* present then the program will exit.             *
 ***************************************************
 ***************************************************
 **************************************************/
@@ -44,6 +49,8 @@ int assign(dict* pD, char *inKey, int inVal){
   //if the dict has not been assigned the key will have
   //  a value == to NULL
   if(pD[index].key == NULL){
+    //printf("Does not contain key\n");
+
     pD[index].key = inKey;
     pD[index].value = inVal;
     return 0;
@@ -56,7 +63,7 @@ int assign(dict* pD, char *inKey, int inVal){
   while(temp->pNext != NULL){
 
     //this means the key already exists
-    if(strcmp(temp->key, inKey) == 1){
+    if(strcmp(temp->key, inKey) == 0){
       temp->value = inVal;
       return 1;
     }
@@ -71,15 +78,56 @@ int assign(dict* pD, char *inKey, int inVal){
   return 1;
 }
 
+//returns boolean telling if the key is present
+int contains(dict* pD, char *inKey){
+  int index = hash(inKey);
+
+  if(pD[index].key == NULL)
+    return 0;
+  dict *temp = &pD[index];
+  while(temp != NULL){
+    //printf("Temp contains: %s\n", temp->key);
+    //printf("Comparing against: %s\n", inKey);
+    if(strcmp(temp->key, inKey) == 0)
+      return 1;
+    temp = temp->pNext;
+  }
+  return 0;
+}
+
+//this function assumes that the dict
+//  actually contains the key and will throw
+//  an error and exit otherwise
+int value(dict *pD, char *inKey){
+  int index = hash(inKey);
+
+  if(pD[index].key == NULL)
+  {
+    printf("Key not found.. Exiting.");
+    exit(8);
+  }
+
+  dict *temp = &pD[index];
+
+  while(temp != NULL){
+    if(strcmp(temp->key, inKey) == 0)
+      return temp->value;
+    temp = temp->pNext;
+  }
+  printf("Key not found.. Exiting.");
+  exit(8);
+}
 
 int main(){
 
   dict *D = calloc(DICTSIZE, sizeof(dict));
+  //printf("%d%d%d%d\n", hash("Kesley"), hash("e"), hash("f"), hash("a") );
 
   assign(D, "Kesley", 7);
 
-  printf("%s", D->key);
-  printf("%d", D->key==NULL);
+  printf("Dict contains Kesley: %d\n", contains(D, "Kesley"));
+  printf("This value of Kesley is %d\n", value(D, "Kesley"));
+  printf("This value of Keegan is %d\n", value(D, "Keegan"));
 
 
 
